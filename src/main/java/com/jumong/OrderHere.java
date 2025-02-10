50860c1a751606f2d53088e7a5c09c1caa69ce25
Jumooong0617 <ulysses.linatoc@lorma.edu>
2025-02-10 08:34:20 +0800

    added objects

--- src/main/java/com/jumong/OrderHere.java
+++ src/main/java/com/jumong/OrderHere.java
@@ -2,13 +2,12 @@ package com.jumong;
 
 public class OrderHere {
     public static void main(String[] args) {
-        // Create two Coffee objects
+
         Coffee coffee1 = new Coffee("Espresso", "Arabica", "Medium", 3.50, "Dark",
                 "Colombia", false, 10, "Espresso Machine");
         Coffee coffee2 = new Coffee("Latte", "Robusta", "Large", 4.00, "Medium",
                 "Brazil", false, 5, "Drip");
 
-        // Call methods on the first coffee
         coffee1.addFlavor("Chocolate");
         coffee1.addFlavor("Nutty");
         coffee1.calculatePrice("Medium");
@@ -18,7 +17,6 @@ public class OrderHere {
         System.out.println("Price after discount: $" + coffee1.getPrice());
         System.out.println("Stock left: " + coffee1.getStock());
 
-        // Call methods on the second coffee
         coffee2.addFlavor("Vanilla");
         coffee2.setDecaf(true);
         coffee2.changeRoastLevel("Light");
