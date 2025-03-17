ab3f3f388b5263cb4182258dcb7d33d2c90d3ef3
Jumooong0617 <ulysses.linatoc@lorma.edu>
2025-03-17 21:53:57 +0800

    may awa ang diyos

---
+++ src/main/java/com/jumong/springbootforms/SpringbootformsApplication.java
@@ -0,0 +1,13 @@
+package com.jumong.springbootforms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootformsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootformsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jumong/springbootforms/SpringbootformsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jumong.springbootforms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootformsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
